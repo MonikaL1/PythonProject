c76d61e88cacfc5f784b935669354599520e87fd
Monika <lumimonika1@gmail.com>
2024-03-17 13:11:35 -1200

    Create columns, images and description

---
+++ main.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Ardit Sulce")
+    content = """ 
+    In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.
+    """
+    st.info(content)
+
