8fb9e9964c3d16a5e5fa5eb06360a85102363b9c
Monika <lumimonika1@gmail.com>
2024-03-17 18:01:49 -1200

    Add navigation menu, add Contact Us page

---
+++ Home.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("images/photo.png")
+
+with col2:
+    st.title("Ardit Sulce")
+    content = """ 
+    Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document 
+    or a typeface without relying on meaningful content. In publishing and graphic design, Lorem ipsum is a placeholder 
+    text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.
+     Lorem ipsum may be used as a placeholder before the final copy is available. In publishing and graphic design, 
+     Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum may be used as a placeholder before the final copy is available.
+    """
+    st.info(content)
+
+content2 = """
+Below you can find some of the app I have built in Python. Feel free to contecat me!
+"""
+st.write(content2)
+
+col3, empty_col, col4 =st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source Code]({row['url']})")
+
+
